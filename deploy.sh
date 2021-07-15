bb1174a4bf74693b7da5daaaf8f1295ccf88e0a3
OpreaMadalin <madalinoprea1992@yahoo.com>
2021-07-15 20:08:02 +0300

    test push

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 ssh root@"$REMOTE_SERVER_IP" 'rm -r /home/Chatroom'
 scp -r ~/Downloads/OpreaMadalin_FinalProject_API root@"$REMOTE_SERVER_IP":/home/Chatroom
 ssh root@"$REMOTE_SERVER_IP" 'bash /home/Chatroom/docker-run.sh'
---
+++ mock-deployment-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+REMOTE_SERVER_IP="$1"
+
+echo "Deploying on $REMOTE_SERVER_IP"
